c2ef5b38c57d850bc73d83cce53e8728cf74d436
Stephen Mathieson <smath23@gmail.com>
2014-12-22 16:34:46 -0500

    keyboard: Support `shift: false`
    More crazy hacks to get `shift: false` working when creating
    `keydown`s.
    
    Without this patch, `e.shiftKey === true` every time.

--- index.js
+++ index.js
@@ -120,6 +120,9 @@ function createKeyboardEvent (type, options) {
     Object.defineProperty(e, 'which', {
       get: function () { return options.key; }
     });
+    Object.defineProperty(e, 'shiftKey', {
+      get: function () { return options.shift; }
+    });
   }
 
   return e;
